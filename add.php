66a1f0408300c93c3d8f76918f8b38a21c1f912f
Elkhan <elkhan.safarov10@yandex.ru>
2019-12-09 19:43:18 +0500

    Загрузка изображений на сайт

--- add.php
+++ add.php
@@ -5,9 +5,13 @@
 	$title = $_POST['title'];
 	$descrMin = $_POST['descr-min'];
 	$description = $_POST['description']; 
+	echo '<pre>';
+	print_r($_FILES);
+	echo '</pre>';
+	move_uploaded_file($_FILES['image']['tmp_name'], 'images/'.$_FILES['image']['name']);
 	$con = connect();
-	$sql = "INSERT INTO info (title, descr_min, description)
-	VALUES ('".$title."', '".$descrMin."', '".$description."')";
+	$sql = "INSERT INTO info (title, descr_min, description, image)
+	VALUES ('".$title."', '".$descrMin."', '".$description."', '".$_FILES['image']['name']."')";
 
 	if(mysqli_query($con, $sql)) {
 		echo "Данные успешно загружены";
--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 	require_once 'core/config.php';
 	require_once 'core/function.php';
 ?>
-<form action="add.php" method="POST">
+<form action="add.php" method="POST" enctype="multipart/form-data">
 	<p>Title: <input type="text" name="title"></p>
 	<p>Min description:</p>
 	<textarea name="descr-min"></textarea>
 	<p>Description:</p>
 	<textarea name="description"></textarea>
+	<p>Photo: <input type="file" name="image"></p>
 	<p><input type="submit" value="add"></p>
 </form>
