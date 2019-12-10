ba7151f4dddfda33af7124e36a55a85d696a195f
Elkhan <elkhan.safarov10@yandex.ru>
2019-12-10 10:56:22 +0500

    Совмещаем форму и обработчик в одном файле

--- add.php
+++ add.php
@@ -1,23 +1,22 @@
 <?php
-	require_once 'core/config.php';
-	require_once 'core/function.php';
+require_once 'core/config.php';
+require_once 'core/function.php';
 
-	$title = $_POST['title'];
-	$descrMin = $_POST['descr-min'];
-	$description = $_POST['description']; 
-	echo '<pre>';
-	print_r($_FILES);
-	echo '</pre>';
-	move_uploaded_file($_FILES['image']['tmp_name'], 'images/'.$_FILES['image']['name']);
-	$con = connect();
-	$sql = "INSERT INTO info (title, descr_min, description, image)
-	VALUES ('".$title."', '".$descrMin."', '".$description."', '".$_FILES['image']['name']."')";
+$title = $_POST['title'];
+$descrMin = $_POST['descr-min'];
+$description = $_POST['description'];
 
-	if(mysqli_query($con, $sql)) {
-		echo "Данные успешно загружены";
-	} else {
-		echo "Ошибка: " . $sql . "<br>" . mysqli_error($con);
-	}
+// echo '<pre>';
+// print_r($_FILES);
+move_uploaded_file($_FILES['image']['tmp_name'], 'images/'.$_FILES['image']['name']);
 
-	close($con);
-?>
+$conn = connect();
+$sql = "INSERT INTO info (title, descr_min, description, image) VALUES ('".$title."', '".$descrMin."', '".$description."', '".$_FILES['image']['name']."')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+close($conn);
