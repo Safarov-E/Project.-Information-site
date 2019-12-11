9153ef68d4c59b097fbc38b2da2924588232e1c0
Elkhan <elkhan.safarov10@yandex.ru>
2019-12-11 22:00:05 +0500

    Пагинация. Разбивка вывода на страницы

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <?php
 require_once 'core/config.php';
 require_once 'core/function.php';
-if(isset($_COOKIE['bd_create_success']) && $_COOKIE['bd_create_success'] != '') {
-	if($_COOKIE['bd_create_success'] == 1) {
-		setcookie('bd_create_success', 1, time()-10);
-		echo "New record created successfully";
-	}
-}
+$conn = connect();
+$data = selectMain($conn);
+$countPage = paginationCount($conn);
+close($conn);
 ?>
-
-<form action="add.php" method="POST" enctype="multipart/form-data">
-    <p>Title: <input type="text" name="title"></p>
-    <p>Min description:</p>
-    <textarea name="descr-min"></textarea>
-    <p>Description:</p>
-    <textarea name="description"></textarea>
-
-    <p>Photo: <input type="file" name="image"></p>
-    
-    <p><input type="submit" value="add"></p>
-</form>
+<?php 
+	$out = '';
+	for($i = 0; $i < count($data); $i++) {
+		$out .= "<img src='/images/{$data[$i]['image']}' width='100'>";
+		$out .= "<h2>{$data[$i]['title']}</h2>";
+		$out .= "<p>{$data[$i]['descr_min']}</p>";
+		$out .= "<p><a href='/arcticle.php?id='{$data[$i]['id']}'>Read more...</a></p>";
+		$out .= '<hr>';
+	}
+	echo $out;
+	for($i = 0; $i < $countPage; $i++) {
+		$j = $i + 1;
+		echo "<a href='/index.php?page={$i}' style='padding: 5px;'>{$j}</a>";
+	}
+?>
